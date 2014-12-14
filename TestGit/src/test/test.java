494eaac9e4597ae81a16626a19ed1b21dfd3c3f3
Pbarbon <pierre.barbon94@gmail.com>
2014-12-14 22:15:49 +0100

    test

--- TestGit/src/test/test.java
+++ TestGit/src/test/test.java
@@ -3,6 +3,7 @@ package test;
 public class test {
 	public test(){
 		System.out.println("hello");
+		System.out.println("Tu es trop fort");
 	}
 	public static void main(String[] args){
 		test t = new test();
